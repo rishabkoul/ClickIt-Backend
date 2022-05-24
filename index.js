1eec90538b3c79b565937fe381d9b06f4df84ffc
Rishab Koul <t-rishab.koul@paytmmoney.com>
2022-05-24 19:02:58 +0530

    heroku-changes

--- index.js
+++ index.js
@@ -15,4 +15,6 @@ app.use(express.json());
 app.use("/api/user", authRoute);
 app.use("/api/nongrouproutes", nonGroupRoutes);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Server running on port 3000")
+);
