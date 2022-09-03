bd56099c80cad06837fb712a5adc38ccb04cfaca
Rishab Koul <t-rishab.koul@paytmmoney.com>
2022-09-03 17:47:10 +0530

    get-your-bookings

--- routes/booking.js
+++ routes/booking.js
@@ -35,4 +35,62 @@ router.post("/book", verify, async (req, res) => {
   }
 });
 
+router.get("/getyourbookings", verify, async (req, res) => {
+  const { page = 1 } = req.query;
+  const { limit = 20 } = req.query;
+  const { query = "" } = req.query;
+
+  try {
+    let bookings;
+    let count;
+    const dates_booked = req.query.dates_booked
+      ? JSON.parse(req.query.dates_booked)
+      : [];
+
+    if (dates_booked.length !== 0) {
+      bookings = await Booking.find({
+        dates_booked: {
+          $in: dates_booked,
+        },
+        booked_by_userId: req.user._id,
+        booked_name: new RegExp(query, "i"),
+      })
+        .limit(limit * 1)
+        .skip((page - 1) * limit)
+        .exec();
+
+      count = await Booking.find({
+        dates_booked: {
+          $in: dates_booked,
+        },
+        booked_by_userId: req.user._id,
+        booked_name: new RegExp(query, "i"),
+      }).countDocuments();
+    } else {
+      bookings = await Booking.find({
+        booked_by_userId: req.user._id,
+        booked_name: new RegExp(query, "i"),
+      })
+        .limit(limit * 1)
+        .skip((page - 1) * limit)
+        .exec();
+
+      count = await Booking.find({
+        booked_by_userId: req.user._id,
+        booked_name: new RegExp(query, "i"),
+      }).countDocuments();
+    }
+
+    res.json({
+      bookings: bookings,
+      totalPages: Math.ceil(count / limit),
+      currentPage: Number(page),
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: `Server Error ${error} Most probably this have caused due to too many filters that there is no your bookings data in the database try removing some filters and trying again`,
+    });
+  }
+});
+
 module.exports = router;
