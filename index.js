50fcea1c6b6719792cd4e4ac157377630a7edd23
rishab_koul <t-rishab.koul@paytmmoney.com>
2023-02-04 18:54:25 +0530

    cors-issue-resolved

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ mongoose.connect(process.env.DB_CONNECT, () => console.log("connected to db!"));
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/api/user", authRoute);
 app.use("/api/nongrouproutes", nonGroupRoutes);
 app.use("/api/agreement", agreementRoutes);
